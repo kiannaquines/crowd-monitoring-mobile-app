2db1e04693e8ee087b2763368662e0255fc1628a
kiannaquines <kjgnaquines@gmail.com>
2024-07-04 00:26:40 +0800

    change main class name to CrowdMonitoringApp

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:crowd/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const CrowdMonitoring());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
