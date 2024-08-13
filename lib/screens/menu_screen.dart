186f86ff5b06288033a608c3f4e6c8dc8dbb140a
kiannaquines <kjgnaquines@gmail.com>
2024-08-13 18:38:19 +0800

    remove fontawesome import
    Signed-off-by: kiannaquines <kjgnaquines@gmail.com>

--- lib/screens/menu_screen.dart
+++ lib/screens/menu_screen.dart
@@ -1,6 +1,5 @@
 import 'package:crowd/utils/colors.dart';
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:icons_plus/icons_plus.dart';
 import 'package:crowd/screens/login_screen.dart';
 
